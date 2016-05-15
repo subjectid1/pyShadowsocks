4b2d776a977953cb61f24ab076b5ded0bc1acb9a
FTwO_O <booopooob@gmail.com>
2016-05-15 12:10:10 +0800

    change log path

--- util/log.py
+++ util/log.py
@@ -2,23 +2,34 @@
 #
 
 import logging
+import logging.handlers
 import os
 
 
-def get_logger(name:str, level)-> logging.Logger:
+def get_logger(name: str, level) -> logging.Logger:
     logger = logging.getLogger(name)
-    logger.setLevel(level)
+    logger.setLevel(level=level)
 
-    utils_dir = os.path.dirname(os.path.abspath(__file__))
-    project_dir = os.path.dirname(utils_dir)
-    log_file = os.path.join(project_dir, 'logs/{}.log'.format(name))
-    if not os.path.exists(log_file):
-        os.makedirs(os.path.dirname(log_file), exist_ok=True)
-        open(log_file, 'ab').close()
-    # TODO: change to use rotatingFileLogger
-    ch = logging.FileHandler(log_file, encoding='utf8')
-    # ch.setLevel(level)
+    log_files = [
+        '/var/logs/pyShadowsocks/{}.log'.format(name),
+        '~/.pyShadowsocks/{}.log'.format(name)
+    ]
+
+    log_file = None
+    for test_file in log_files:
+        try:
+            test_file = os.path.expanduser(test_file)
+            os.makedirs(os.path.dirname(test_file), exist_ok=True)
+            open(test_file, 'ab').write(b'xxxxx')
+        except PermissionError:
+            continue
+        else:
+            log_file = test_file
+            break
+
+    assert (log_file is not None)
+    handler = logging.handlers.TimedRotatingFileHandler(log_file, encoding='utf8', when='D')
     formatter = logging.Formatter('[%(asctime)s-%(levelname)s]:%(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
