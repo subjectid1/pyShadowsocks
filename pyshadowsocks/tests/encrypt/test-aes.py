b88e84f87d1857abff77353adffa2c08ac838ed9
FTwOoO <booopooob@gmail.com>
2017-09-30 13:57:36 +0800

    test

--- pyshadowsocks/tests/encrypt/test-aes.py
+++ pyshadowsocks/tests/encrypt/test-aes.py
@@ -1,34 +1,8 @@
-# The MIT License (MIT)
-#
-# Copyright (c) 2014 Richard Moore
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+from encrypt.aes import Decrypter, Encrypter, PADDING_NONE, encrypt_stream, decrypt_stream, append_PKCS7_padding, \
+    strip_PKCS7_padding
+import encrypt.aes as pyaes
 
-
-import sys
-
-sys.path.append('../pyaes')
-
-from pyaes import *
-from pyaes.aes import Decrypter, Encrypter, PADDING_NONE, encrypt_stream, decrypt_stream
-import pyaes
-from Crypto.Cipher import AES as KAES
+from crypto.Cipher import AES as KAES
 from Crypto.Util import Counter as KCounter
 import time
 import os
@@ -272,7 +246,6 @@ class ConfigTest(unittest.TestCase):
             print("  Native better by: %dx" % better)
 
     def test_PKCS7(self):
-        from pyaes.aes import append_PKCS7_padding, strip_PKCS7_padding
 
         byte = 'A'
 
